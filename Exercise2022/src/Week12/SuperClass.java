cf646f21c69e32e058f4b8647619f7f50100ffcd
pensyarah <pensyarah@FSKTM-DK1.fsktm.edu>
2023-01-09 11:02:43 +0800

    Week 03 End

---
+++ Exercise2022/src/Week12/SuperClass.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week12;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pensyarah
+ */
+public class SuperClass {
+    
+    private String name;
+    private int age;
+    public int ageTwo;
+
+    public SuperClass() {
+        System.out.println("Constructor SuperClass");
+    }
+
+    public SuperClass(String name) {
+        this.name = name;
+    }
+
+    public SuperClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public SuperClass(String name, int age, int ageTwo) {
+        this.name = name;
+        this.age = age;
+        this.ageTwo = ageTwo;
+    }
+    
+    public void enterName(){
+        Scanner input =  new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        this.name=input.nextLine();
+        System.out.print("Enter your age: ");
+        this.age = input.nextInt();
+    }
+    
+    public void printName(){
+        System.out.println("Name :" + this.name);
+    }
+    
+    public void printAge(){
+        System.out.println("Age (From Class) :" + this.age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+    public int inputAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void printNewAge(){
+        System.out.println("New Age (From Class) :" + this.ageTwo);
+    }
+    
+    public void printNewAge(int ageTwo){
+        System.out.println("New Age (From Class) (Overloading) :" + ageTwo);
+    }
+}
---
+++ Exercise2022/src/Week12/W12E01.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week12;
+
+/**
+ *
+ * @author pensyarah
+ */
+public class W12E01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        SuperClass classOne =  new SuperClass();
+        classOne.enterName();
+        classOne.printName();
+        classOne.printAge();
+        classOne.ageTwo = 20;
+        //System.out.println("The new age : " + classOne.ageTwo);
+        classOne.printNewAge();
+        
+        SuperClass classTwo =  new SuperClass("Badrul Anuar");
+        classTwo.setAge(22);
+        classTwo.printName();
+        //classTwo.printAge();
+        System.out.println("Age (Main Class) :" + classTwo.getAge());
+        classTwo.printNewAge(65);
+        
+        SuperClass classThree =  new SuperClass("Ahmed", 28);
+        classThree.printName();
+        classThree.printAge();
+        System.out.println("Print Again :" + classThree.inputAge());
+        
+        
+    }
+    
+}
